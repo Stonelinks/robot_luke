ffac539fc35d8409c481ff79608458c43e884543
Lucas Doyle <ldoyle@airware.com>
2017-05-09 16:53:28 -0700

    add shut up command

--- index.js
+++ index.js
@@ -1,93 +1,100 @@
 /**
  * Created by ld on 2/8/16.
  */
-var Botkit = require('botkit');
-var Cleverbot = require('cleverbot-node');
-var HerokuKeepalive = require('@ponko2/botkit-heroku-keepalive').default;
+var Botkit = require('botkit')
+var Cleverbot = require('cleverbot-node')
+var HerokuKeepalive = require('@ponko2/botkit-heroku-keepalive').default
 
-var fs = require('fs');
-var path = require('path');
-var handlersDir = './handlers';
+var fs = require('fs')
+var path = require('path')
+var handlersDir = './handlers'
 
 // Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN;
+var slackToken = process.env.SLACK_TOKEN
 if (!slackToken) {
-    console.error('SLACK_TOKEN is required!');
-    process.exit(1)
+  console.error('SLACK_TOKEN is required!')
+  process.exit(1)
 }
 
-var controller = Botkit.slackbot();
+var controller = Botkit.slackbot()
 
-var cleverbotToken = process.env.CLEVERBOT_TOKEN;
-var cleverbot = new Cleverbot;
-cleverbot.configure({botapi: cleverbotToken});
-    
-var herokuKeepalive;
+var cleverbotToken = process.env.CLEVERBOT_TOKEN
+var cleverbot = new Cleverbot
+cleverbot.configure({botapi: cleverbotToken})
+
+var herokuKeepalive
 controller.setupWebserver(process.env.PORT || 8080, function (err, webserver) {
-    herokuKeepalive = new HerokuKeepalive(controller);
-});
+  herokuKeepalive = new HerokuKeepalive(controller)
+})
 
 var bot = controller.spawn({
-    token: slackToken
-});
+  token: slackToken
+})
 
 bot.startRTM(function (err, bot, payload) {
-    if (err) {
-        throw new Error('Could not connect to Slack')
+  if (err) {
+    throw new Error('Could not connect to Slack')
+  }
+
+  fs.readdirSync(path.join(__dirname, handlersDir)).forEach(function (handlerName) {
+    console.log(`booting ${handlerName}`)
+    require(`${handlersDir}/${handlerName}`)(controller)
+  })
+
+  controller.hears('help.*', ['direct_message', 'direct_mention'], function (bot, message) {
+    bot.reply(message, 'This is intentionally left blank')
+  })
+
+  var talkClever = function (bot, message, prefix, messageText) {
+    prefix = prefix || ''
+    var messageText = messageText || message.text
+    cleverbot.write(messageText, function (response) {
+      try {
+        bot.reply(message, prefix + ' ' + response.output)
+      } catch (e) {
+        console.error(e)
+      }
+    })
+  }
+
+  var cleverIntervals = {}
+
+  controller.hears('talk to (.*)', ['direct_message', 'direct_mention'], function (bot, message) {
+    var matches = message.text.match(/talk to (.*)/i)
+    var user = matches[1]
+
+    bot.reply(message, user + " we're best friends now")
+    cleverIntervals[user] = setInterval(function () {
+      talkClever(bot, message, user, 'go on a rant')
+    }, 20 * 1000)
+  })
+
+  controller.hears('stop talking to (.*)', ['direct_message', 'direct_mention'], function (bot, message) {
+    var matches = message.text.match(/stop talking to (.*)/i)
+    var user = matches[1]
+
+    bot.reply(message, user + " we're no longer best friends")
+    clearInterval(cleverIntervals[user])
+  })
+
+  controller.hears('shut up', ['direct_message', 'direct_mention'], function (bot, message) {
+    bot.reply(message, 'k')
+    for (var i in cleverIntervals) {
+      clearInterval(cleverIntervals[i])
     }
+  })
 
-    fs.readdirSync(path.join(__dirname, handlersDir)).forEach(function (handlerName) {
-        console.log(`booting ${handlerName}`)
-        require(`${handlersDir}/${handlerName}`)(controller);
-    })
+  controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
+    talkClever(bot, message)
+  })
 
-    controller.hears('help.*', ['direct_message', 'direct_mention'], function (bot, message) {
-        bot.reply(message, 'This is intentionally left blank')
-    });
-
-    var talkClever = function(bot, message, prefix, messageText) {
-      prefix = prefix || ''
-      var messageText = messageText || message.text
-      cleverbot.write(messageText, function (response) {
-        try {
-          bot.reply(message, prefix + ' ' + response.output);
-        } catch (e) {
-          console.error(e)
-        }
-      })
-    }
-
-    var cleverIntervals = {}
-
-    controller.hears('talk to (.*)', ['direct_message', 'direct_mention'], function (bot, message) {
-      var matches = message.text.match(/talk to (.*)/i);
-      var user = matches[1];
-      
-      bot.reply(message, user + ' we\'re best friends now')
-      cleverIntervals[user] = setInterval(function() {
-        talkClever(bot, message, user, 'go on a rant')
-      }, 20 * 1000)
-    })
-
-    controller.hears('stop talking to (.*)', ['direct_message', 'direct_mention'], function (bot, message) {
-      var matches = message.text.match(/stop talking to (.*)/i);
-      var user = matches[1];
-      
-      bot.reply(message, user + ' we\'re no longer best friends')
-      clearInterval(cleverIntervals[user])
-    })
-
-    controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-      talkClever(bot, message)
-    })
-
-    herokuKeepalive.start();
-    console.log('booted')
-});
+  herokuKeepalive.start()
+  console.log('booted')
+})
 
 //
-//controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-//    var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.';
+// controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+//    var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
 //    var attachments = [{
 //        fallback: text,
 //        pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
@@ -96,12 +103,12 @@ bot.startRTM(function (err, bot, payload) {
 //        title_link: 'https://beepboophq.com/',
 //        text: text,
 //        color: '#7CD197'
-//    }];
+//    }]
 //
 //    bot.reply(message, {
 //        attachments: attachments
 //    }, function (err, resp) {
 //        console.log(err, resp)
 //    })
-//});
+// })
 //
